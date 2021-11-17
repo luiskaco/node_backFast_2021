45559cb9b82ad2df836812921178b041ebb9fc7b
Luis Gomez <luiskaco@gmail.com>
2021-11-17 00:50:03 -0500

    Verificando el token de google

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -9,6 +9,8 @@ const Usuario = require('../models/usuario');
 
 // Importamos helpr con la funcion de generar json
 const { generarJWT } = require('../helpers/generarJWT');
+const { googleVerify } = require('../helpers/google-verif');
+const usuario = require('../models/usuario');
 
 
 
@@ -73,13 +75,70 @@ const login = async (req, res = response) => {
 
     // Nota: igualamos el res = response, solo para que VScode nos ayude con el tipeado
 const googleSingIn = async (req, res = response) => {
+    
+    // Capturando el token
     const { token_id } = req.body;
 
+    // Extraemos informacion
+    try {
+        const {correo, img, nombre} = await googleVerify(token_id);
+        // console.log(googleUser)
 
-   res.json({
-       msg: 'Todo Bien',
-       token_id
-   })
+        // verificar si ya existe el usuario
+        let usuario = await Usuario.findOne({correo});
+
+            if (! usuario ){
+                // Crear usuario
+                const data = {
+                    nombre, 
+                    correo,
+                    password: 'ghjklkjhgesdrtyujiklñ',
+                    google:true
+                };
+
+                usuario = new Usuario(data);
+                await usuario.save();
+
+                res.json({
+                    msg: 'Todo Bien Google Singnins',
+              
+                    usuario
+                })
+
+            }
+
+            // Si el usuario de BD tiene el stado de false
+
+            if(!usuario.estado){
+                return res.status(401).json({
+                    msg: 'Hablar con el administrador, usuario bloqueado'
+                });
+            }
+
+
+               // To do : Generar JWT
+                const token = await generarJWT(usuario._id);
+
+                console.log(usuario)
+
+                res.json({
+                    msg: 'Todo Bien Google Singnins',
+                    token,
+                 
+                })
+
+    } catch (error) {
+        console.log(error)
+
+        res.status(400).json({
+            ok:false,
+            msg:'El token no se pudo verificar'
+        });
+    }
+
+
+
+  
 }
  
 module.exports = {
